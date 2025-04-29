c0a3045ae48f057878ec154a61602056726e3b44
totorien71 <victorien.soufflot@gmail.com>
2025-04-29 14:13:51 +0200

    x,

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def contact():
 
 @app.route("/a-propos")
 def a_propos():
-    return render_template("a_propos.html")
+    return render_template("apropos.html")
 
 @app.route("/morpion")
 def morpion():
