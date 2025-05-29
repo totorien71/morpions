bedea4f2a3317351509493fca10fcb6a9c0c784e
totorien71 <victorien.soufflot@gmail.com>
2025-05-29 17:19:26 +0200

    xstg

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,5 +18,6 @@ def a_propos():
 def morpion():
     return render_template("morpion.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
