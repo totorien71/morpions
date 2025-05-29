4ba0a835f891b914a08d112d43e2e39acf1bcea1
totorien71 <victorien.soufflot@gmail.com>
2025-05-29 17:25:02 +0200

    d

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def a_propos():
 @app.route("/morpion")
 def morpion():
     return render_template("morpion.html")
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
